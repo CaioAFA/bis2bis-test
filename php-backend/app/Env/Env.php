52e840cfc7a9689f92fae23353880e9df282644b
Caio Arrabal <caio.arrabal@e-thinkers.com.br>
2021-11-01 21:36:03 -0300

    Adicionado arquivo .env do php.

---
+++ php-backend/app/Env/Env.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Env;
+
+class Env {
+  /**
+   * Database host
+   * @var string
+   */
+  public static $DB_HOST = 'mysql';
+
+  /**
+   * Database name
+   * @var string
+   */
+  public static $DB_NAME = 'cb_blog';
+
+  /**
+   * Database user
+   * @var string
+   */
+  public static $DB_USER = 'root';
+
+  /**
+   * Database user password
+   * @var string
+   */
+  public static $DB_PASS = 'root_password';
+}
